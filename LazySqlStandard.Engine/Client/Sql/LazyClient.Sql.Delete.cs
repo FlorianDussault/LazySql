5d2b86939e9e15dced83c4ae82ae059c6bf8a9a4
Florian Dussault <floriandussault@hotmail.com>
2022-10-31 06:57:44 +0100

    Add missing files

---
+++ LazySqlStandard.Engine/Client/Sql/LazyClient.Sql.Delete.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using LazySql.Engine.Client.Query;
+using LazySql.Engine.Connector;
+using LazySql.Engine.Definitions;
+
+namespace LazySql.Engine.Client
+{
+    // ReSharper disable once ClassCannotBeInstantiated
+    public sealed partial class LazyClient
+    {
+        #region Truncate
+
+        public static void Truncate<T>(bool reseed = false) where T : LazyBase => Instance.InternalTruncate<T>(reseed);
+
+        private void InternalTruncate<T>(bool reseed = false) where T : LazyBase
+        {
+            CheckInitialization<T>(out TableDefinition tableDefinition);
+
+            using (SqlConnector sqlConnector = Open())
+            {
+                if (reseed)
+                {
+                    InternalDelete<T>();
+                    sqlConnector.ExecuteQuery($"DBCC CHECKIDENT ('{tableDefinition.Table.TableName}', RESEED, 0)");
+                }
+                else
+                {
+                    sqlConnector.ExecuteQuery($"TRUNCATE TABLE {tableDefinition.Table.TableName}");
+                }
+            }
+        }
+
+        #endregion
+
+        #region Delete
+
+        public static void Delete<T>(Expression<Func<T, bool>> expression = null) where T : LazyBase => Instance.InternalDelete<T>(expression);
+
+        private void InternalDelete<T>(Expression<Func<T,bool>> expression = null) where T : LazyBase
+        {
+            CheckInitialization<T>(out TableDefinition tableDefinition);
+
+            QueryBuilder queryBuilder = new QueryBuilder(tableDefinition);
+            queryBuilder.Append($"DELETE FROM {tableDefinition.Table.TableName}");
+
+            if (expression != null)
+            {
+                queryBuilder.Append(" WHERE ", expression);
+            }
+
+            ExecuteNonQuery(queryBuilder);
+        }
+
+
+        public static void Delete<T>(T obj) where T : LazyBase => Instance.InternalDelete(obj);
+
+
+        public void InternalDelete<T>(T obj) where T : LazyBase
+        {
+            CheckInitialization<T>(out TableDefinition tableDefinition);
+
+            tableDefinition.GetColumns(out _, out _, out _, out IReadOnlyList<ColumnDefinition> primaryKeys);
+
+            BinaryExpression binaryExpression = null;
+            foreach (ColumnDefinition primaryKey in primaryKeys)
+            {
+                var objExpression = Expression.Parameter(typeof(T), "source");
+                var member = Expression.Property(objExpression, primaryKey.PropertyInfo);
+                var value = Expression.Constant(primaryKey.PropertyInfo.GetValue(obj));
+                BinaryExpression childExpression = Expression.Equal(member, value);
+                if (binaryExpression == null)
+                {
+                    binaryExpression = childExpression;
+                    continue;
+                }
+                binaryExpression = Expression.AndAlso(binaryExpression, childExpression);
+            }
+
+            QueryBuilder queryBuilder = new QueryBuilder(tableDefinition);
+            queryBuilder.Append($"DELETE FROM {tableDefinition.Table.TableName} WHERE ");
+            queryBuilder.Append(binaryExpression);
+
+            using (SqlConnector sqlConnector = Open())
+                sqlConnector.ExecuteQuery(queryBuilder.GetQuery(), queryBuilder.GetArguments());
+        }
+
+
+        #endregion
+    }
+}
---
+++ LazySqlStandard.Engine/Client/Sql/LazyClient.Sql.Insert.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LazySql.Engine.Client.Query;
+using LazySql.Engine.Definitions;
+using LazySql.Engine.Enums;
+
+namespace LazySql.Engine.Client
+{
+    // ReSharper disable once ClassCannotBeInstantiated
+    public sealed partial class LazyClient
+    {
+        #region Insert
+
+        public static void Insert<T>(T obj) where T : LazyBase => Instance.InternalInsert(obj);
+
+        private void InternalInsert<T>(T obj) where T : LazyBase
+        {
+            CheckInitialization<T>(out TableDefinition tableDefinition);
+            tableDefinition.GetColumns(out _, out IReadOnlyList<ColumnDefinition> columns, out _,
+                out IReadOnlyList<ColumnDefinition> primaryKeys);
+
+            ColumnDefinition autoIncrementColumn = primaryKeys.FirstOrDefault(c => c.PrimaryKey.AutoIncrement);
+
+            QueryBuilder queryBuilder = new QueryBuilder(tableDefinition);
+            queryBuilder.Append($"INSERT INTO {tableDefinition.Table.TableName}");
+
+            queryBuilder.Append(
+                $" ({string.Join(", ", columns.Where(c => c.Column.SqlType != SqlType.Children).Select(c => c.Column.SqlColumnName))})");
+
+            if (autoIncrementColumn != null)
+                queryBuilder.Append($" output INSERTED.{autoIncrementColumn.Column.SqlColumnName}");
+
+
+            List<string> values = columns.Where(c => c.Column.SqlType != SqlType.Children).Select(columnDefinition => queryBuilder.RegisterArgument(columnDefinition.Column.SqlType ,columnDefinition.GetValue(obj))).ToList();
+            queryBuilder.Append($" VALUES ({string.Join(", ", values)})");
+
+            if (autoIncrementColumn != null)
+            {
+                object output = ExecuteScalar(queryBuilder);
+                autoIncrementColumn.PropertyInfo.SetValue(obj, output);
+            }
+            else
+            {
+                ExecuteNonQuery(queryBuilder);
+            }
+        }
+
+        #endregion
+    }
+}
